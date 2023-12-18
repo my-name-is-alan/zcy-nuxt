5793fc718d91f5e308cb0a86058ec308d5358189
慕容云海重庆分海 <zy94xhn.997@gmail.com>
2023-12-18 16:04:55 +0800

    fix: 去除log

--- composeable/useIndexVideo.ts
+++ composeable/useIndexVideo.ts
@@ -1,7 +1,7 @@
 
 export function useIndexVideo() {
     const list = shallowRef<string[]>([]);
-    list.value = Array.from({ length: 799 }, (_, i) => `https://cdn.izhong.xyz/zcy${i}.webp`);
+    list.value = Array.from({ length: 799 }, (_, i) => `https://cdn.izhong.xyz/zcy${i+1}.webp`);
     return {
         list
     };
